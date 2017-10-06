b935974a931295c53c7a332532dea5afeef8da17
monika <monika.solanki@gmail.com>
2017-10-06 12:50:52 +0100

    writtem test and app

--- src/test/java/com/tdd/AppTest.java
+++ src/test/java/com/tdd/AppTest.java
@@ -3,6 +3,8 @@ package com.tdd;
 
 import org.junit.Assert;
 import org.junit.Test;
+import com.tdd.App;
+import static org.junit.Assert.*;
 
 /**
  * Unit test for simple App.
@@ -13,6 +15,19 @@ public class AppTest{
     public void shouldReturnTrue(){
         Assert.assertTrue(1 == 1);
 
+    }
+
+    @Test
+    public void checkNumberDivisbleByThreeAndFive()
+    {
+        App app = new App();
+        assertEquals("FizzBuzz", app.checkNumber(6));
+    }
+
+    public void chedkNumberNotDivisbleByThreeAndFive()
+    {
+        App app = new App();
+        assertEquals("6", app.checkNumber(6));
     }
 
 
